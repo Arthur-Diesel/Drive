e8488823e52cc79d02d282b4b796170958e3cdb4
Arthur Diesel <arthurdiesel@outlook.com>
2021-09-23 14:53:39 -0300

    +Adição de Imgs. / +Visualização de Imgs.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const crypto = require('crypto')
 const express = require('express')
 const multer  = require('multer')
 const mongoose = require('mongoose')
@@ -7,17 +9,57 @@ require('dotenv/config')
 
 const port = 3000
 
-const app = express()
-app.use(express.static(__dirname + '/public'))
+var app = express()
+app.use(express.static('public/img'))
 app.use(bodyParser.urlencoded({extended: false}))
 
-const upload = multer({ dest: 'uploads/' })
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+var upload = multer({ dest: __dirname + '/public/img/'})
 
 app.get('/', (req, res) =>
 {
     res.status(200).sendFile(__dirname + '/public/views/index.html')
+})
+
+app.post('/upload', upload.single('image'), (req, res) =>{
+    
+    var file = req.file
+
+    check_jpg = file['originalname'].includes('.jpg') 
+    check_png = file['originalname'].includes('.png') 
+    check_gif = file['originalname'].includes('.gif')
+
+    if(check_jpg || check_png || check_gif)
+    {
+
+        if(check_jpg)
+        {
+            var extension = '.jpg'
+        }
+        if(check_png)
+        {
+            var extension = '.png'
+        }
+        if(check_gif)
+        {
+            var extension = '.gif'
+        }
+        var new_file_name = crypto.randomBytes(32).toString('hex') + extension
+        fs.renameSync(`${file['destination']}${file['filename']}`, `${file['destination']}/${new_file_name}`);
+        res.status(201).json({ original_name: file['originalname'], new_name:new_file_name, type: file['mimetype'], size: file['size'], destination: `http://localhost:3000/${new_file_name}` })
+    }
+    else
+    {
+        res.status(400).json({ error: "File isn't img or gif!" })
+        try 
+        {
+            fs.unlinkSync(file['destination'] + file['filename'])
+        } 
+        catch(err) 
+        {
+            console.log("Houve um erro na remoção do arquivo: " + file['filename'])
+        }
+    }
+
 })
 
 app.listen(port, () => {
